9313ac754539f42791c203ec7a15048fcbac7b71
Anthonia <Anthonia@DESKTOP-09RNETR>
2024-02-26 18:30:45 +1300

    Used required annotations in model

--- UserManagement/Models/User.cs
+++ UserManagement/Models/User.cs
@@ -1,15 +1,31 @@
 ﻿
 
+using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
+
 namespace UserManagement.Models
 {
     public class User
     {
+        [SwaggerSchema(ReadOnly = true)]
         public int Id { get; set; }
+
+        [Required]
         public string? Firstname { get; set; }
+
+        [Required]
         public string? Lastname { get; set; }
+
+        [Required]
         public string? Email { get; set; }
+
+        [Required]
         public string? PhoneNumber { get; set; }
+
+        [Required]
         public DateOnly DOB { get; set; }
+
+        [Required]
         public string? Password { get; set; }
 
     }
--- UserManagement/Program.cs
+++ UserManagement/Program.cs
@@ -10,8 +10,10 @@ builder.Services.AddDbContext<DataContext>(options => options.UseMySql(connectio
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(options =>
+{
+    options.EnableAnnotations();
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
